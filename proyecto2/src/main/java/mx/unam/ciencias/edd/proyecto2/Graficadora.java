a4e5a69bcc36ed656cc3be0df8d173df91322f45
Emilio Martinez <e.martinez.g@ciencias.unam.mx>
2025-04-16 08:27:08 -0600

    Nopuedeser

--- proyecto2/src/main/java/mx/unam/ciencias/edd/proyecto2/Graficadora.java
+++ proyecto2/src/main/java/mx/unam/ciencias/edd/proyecto2/Graficadora.java
@@ -6,6 +6,11 @@ package mx.unam.ciencias.edd.proyecto2;
  * Clase abstracta para graficadoras.
  */
 public abstract class Graficadora {
+
+    /**
+     * El lienzo cuyas dimensiones se usarán para graficar.
+     */
+    protected Lienzo lienzo;
     
     /**
      * Método para obtener la representación en cadena de la
---
+++ proyecto2/src/main/java/mx/unam/ciencias/edd/proyecto2/Lienzo.java
@@ -0,0 +1,115 @@
+package mx.unam.ciencias.edd.proyecto2;
+
+/**
+ * Clase para calcular el tamaño del lienzo.
+ */
+public class Lienzo {
+
+    /**
+     * Valor máximo de Y.
+     */
+    private int alto;
+
+    /**
+     * Valor máximo de X.
+     */
+    private int ancho;
+
+    /**
+     * La estructura con la que se calculará el tamaño del
+     * lienzo.
+     */ 
+    private Estructura estructura;
+
+    /**
+     * Calcula el tamaño del lienzo a partir de una
+     * estructura y un número importante que depende de esta.
+     * @param estructura la estructura a partir de la cual se
+     *        calculará el tamaño.
+     * @param decisor, el número importante. Este puede ser
+     *        el número de elementos o la altura dependiendo.
+     *        del caso.
+     */
+    public Lienzo(Estructura estructura, int decisor) {
+	this.estructura = estructura;
+	calculaTamano(decisor);
+    }
+	
+    /**
+     * Regresa el valor máximo de Y.
+     * @return el valor máximo de Y.
+     */
+    public int getAlto() {
+	return alto;
+    }
+    
+    /**
+     * Regresa el valor máximo de X.
+     * @return el valor máximo de X.
+     */
+    public int getAncho() {
+	return ancho;
+    }
+
+    /**
+     * Calcula los valores de altura y ancho.
+     */
+    private void calculaTamano(int decisor) {
+	switch (estructura) {
+	case Estructura.PILA : tamanoPila(int decisor);
+	    break;
+	case Estructura.COLA : tamanoCola(int decisor);
+	    break;
+	case Estructura.LISTA : tamanoLista(int decisor);
+	    break;
+	case Estructura.GRAFICA : tamanoGrafica(int decisor);
+	    break;
+	case default : tamanoArbol();
+	}
+    }
+
+    /**
+     * Calcula el tamaño del lienzo para una pila a partir de
+     * su número de elementos.
+     * @param decisor el número de elementos en la pila.
+     */
+    private void tamanoPila(int decisor) {
+
+    }
+
+    /**
+     * Calcula el tamaño del lienzo para una cola a partir de
+     * su número de elementos.
+     * @param decisor el número de elementos en la cola.
+     */
+    private void tamanoCola(int decisor) {
+
+    }
+
+    /**
+     * Calcula el tamaño del lienzo para una lista a partir
+     * de su número de elementos.
+     * @param decisor el número de elementos en la lista.
+     */
+    private void tamanoLista(int decisor) {
+
+    }
+
+    /**
+     * Calcula el tamaño del lienzo para una gráfica a partir
+     * de su número de elementos.
+     * @param decisor el número de elementos de la gráfica.
+     */
+    private void tamanoGrafica(int decisor) {
+
+    }
+
+    /**
+     * Calcula el tamaño del lienzo para un árbol binario a
+     * partir de su altura.
+     * @param decisor la altura del árbol.
+     */
+    private void tamanoArbol(int decisor) {
+
+    }
+}
